d692da47f7d609eb517c4ed7326532bbb22e9023
10cheon00 <10cheon00@gmail.com>
2023-11-17 22:41:02 +0900

    init

---
+++ src/main/java/com/feeling/app/FeelingAppApplication.java
@@ -0,0 +1,13 @@
+package com.feeling.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feeling/app/FeelingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feeling.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

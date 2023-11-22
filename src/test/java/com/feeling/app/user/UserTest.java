ed732713828c7d3a87aa9f5aed2ae660c7223283
10cheon00 <10cheon00@gmail.com>
2023-11-22 17:27:55 +0900

    test: change UserTest to use @SpringBootTest
    @SpringBootTest는 통합 테스트를 위해 사용하는 것이고 @WebMvcTest는
    단순히 Controller를 테스트하기 위한 것이다.
    관련 출처 : https://dongjuppp.tistory.com/83
    
    todo: implement jwt token tests

--- src/test/java/com/feeling/app/user/UserTest.java
+++ src/test/java/com/feeling/app/user/UserTest.java
@@ -3,25 +3,24 @@ package com.feeling.app.user;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.feeling.app.entity.User;
 import com.feeling.app.service.UserService;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class UserTest {
     @Autowired
     private MockMvc mockMvc;
-    @MockBean
-    private UserService userService;
 
     private String usersURI = "/api/v1/users";
 
@@ -29,12 +28,26 @@ public class UserTest {
     public void 유저_생성() throws Exception {
         User user = new User("username", "password");
 
-        given(userService.createUser(any())).willReturn(user);
+        mockMvc.perform(post(usersURI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(user)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value(user.getName()));
+    }
+
+    @Test
+    public void 중복유저_생성() throws Exception {
+        User user = new User("username", "password");
 
         mockMvc.perform(post(usersURI)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(new ObjectMapper().writeValueAsString(user)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.name").value(user.getName()));
+
+        mockMvc.perform(post(usersURI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(user)))
+                .andExpect(status().isBadRequest());
     }
 }

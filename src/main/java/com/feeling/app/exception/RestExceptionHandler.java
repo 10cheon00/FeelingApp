f7bc49c1aa6a1e313f00f9d1fbae3dd5180c292d
10cheon00 <10cheon00@gmail.com>
2023-11-23 21:39:19 +0900

    feat: create exception handler

--- src/main/java/com/feeling/app/exception/RestExceptionHandler.java
+++ src/main/java/com/feeling/app/exception/RestExceptionHandler.java
@@ -1,2 +1,23 @@
-package com.feeling.app.exception;public class RestExceptionHandler {
+package com.feeling.app.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<Object> errorHandler(IllegalArgumentException exception, WebRequest request){
+        String responseString = "Illegal Argument.";
+        return handleExceptionInternal(
+                exception,
+                responseString,
+                new HttpHeaders(),
+                HttpStatus.BAD_REQUEST,
+                request);
+    }
 }
